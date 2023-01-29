c7fa79d397149b5ca032e0a09df8b34b3c2fa655
DLLM <DLLM@gft.com>
2023-01-28 21:11:55 -0300

    subindo refatoracao para excluir do eclipse

--- src/main/java/com/example/demo/step/SpringBtachStepConfig.java
+++ src/main/java/com/example/demo/step/SpringBtachStepConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.step;
 
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,11 @@ public class SpringBtachStepConfig {
 	private StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Step imprimeOlaStep(Tasklet SpringBtachTasklet) {
+	@StepScope
+	public Step imprimeOlaStep(Tasklet tasklet) {
 		return stepBuilderFactory
 				.get("imprimeOlaStep")
-				.tasklet(SpringBtachTasklet)
+				.tasklet(tasklet)
 				.build();
 	}
 
