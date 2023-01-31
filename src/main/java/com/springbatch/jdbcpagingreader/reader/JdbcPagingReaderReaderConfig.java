7e02699cb6fab52e9af87bd4023e2f176833ccb3
DLLM <DLLM@gft.com>
2023-01-31 12:14:07 -0300

    Subindo o batch com paging paginacao

---
+++ src/main/java/com/springbatch/jdbcpagingreader/reader/JdbcPagingReaderReaderConfig.java
@@ -0,0 +1,44 @@
+package com.springbatch.jdbcpagingreader.reader;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import com.springbatch.jdbcpagingreader.dominio.Cliente;
+
+@Configuration
+public class JdbcPagingReaderReaderConfig {
+	@Bean
+	public JdbcPagingItemReader<Cliente> jdbcPagingReader(
+			@Qualifier("appDataSource") DataSource dataSource,PagingQueryProvider queryProvider) {
+		return new JdbcPagingItemReaderBuilder<Cliente>()
+				.name("jdbcPagingReader")
+				.dataSource(dataSource)
+				.queryProvider(queryProvider)
+				.pageSize(1)
+				.rowMapper(new BeanPropertyRowMapper<Cliente>(Cliente.class))
+				.build();
+	}
+	
+	@Bean
+	public SqlPagingQueryProviderFactoryBean queryProvider(@Qualifier("appDataSource") DataSource dataSource) {
+		//são os componente que realizara as consult5as 
+		SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+		queryProvider.setDataSource(dataSource);
+		queryProvider.setSelectClause("SELECT *");
+		queryProvider.setFromClause("FROM cliente");
+		queryProvider.setSortKey("email");
+		
+		return queryProvider;
+		
+		
+	}
+	
+}
